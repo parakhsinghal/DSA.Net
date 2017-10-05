736572061ab8cf224867e89556fe1fae06f78985
parakhsinghal <parakh@thinkingcog.com>
2017-10-05 16:49:50 +0530

    Added programming to the SimpleArray class.

--- DataStructures/DoublyLinkedList/Node.cs
+++ DataStructures/DoublyLinkedList/Node.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DataStructures.DoublyLinkedList
 {
-    publis sealed class Node
+    public sealed class Node
     {
     }
 }
--- DataStructures/SimpleArray/SimpleArray.cs
+++ DataStructures/SimpleArray/SimpleArray.cs
@@ -12,26 +12,83 @@ namespace DataStructures.SimpleArray
     public class SimpleArray<T>
     {
         T[] array;
+        int noOfElements;
 
         public SimpleArray(int arraySize)
         {
-            T[] array = new T[arraySize];
-            
+            array = new T[arraySize];
+            noOfElements = 0;
         }
 
         public bool Add(T item)
         {
-            throw new NotImplementedException();
+            // Addition can only happen in an instantiated array
+            // and there needs to be space in the array for an item to be inserted.
+            if (array.Length > 0 && noOfElements <= array.Length)
+            {
+                array[noOfElements] = item;
+                noOfElements++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool Remove(T item)
         {
-            throw new NotImplementedException();
+            // Removal of an item can take place on an instantiated array.            
+            if (array.Length > 0)
+            {
+                int itemIndex = Find(item);
+
+                if (itemIndex > 0)
+                {
+                    for (int i = itemIndex; i < noOfElements; i++)
+                    {
+                        if (i == noOfElements-1)
+                        {
+                            break;
+                        }
+                        array[i] = array[i + 1];
+                    }
+                    // Reduce the count by 1
+                    // Note that the value in the last cell is still there but not accessible 
+                    // via find, as the noOfElements has been reduced by 1.
+                    noOfElements--;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public int Find(T item)
         {
-            throw new NotImplementedException(); // Implement simple search
+            // Search needs to be carried out on an instantiated array
+            if (array.Length > 0)
+            {
+                // Simple search
+                for (int i = 0; i < noOfElements; i++)
+                {
+                    if (array[i].Equals(item))
+                    {
+                        return i;
+                    }
+                }
+                return -1;
+            }
+            else
+            {
+                return -1;
+            }
         }
     }
 }
