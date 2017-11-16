942d3a71bf54a8545b166ece5dd0105d8e73071c
parakhsinghal <parakh@thinkingcog.com>
2017-11-17 00:50:39 +0530

    Started working on binary tree.

--- DataStructures/BinaryTree/BinaryTree.cs
+++ DataStructures/BinaryTree/BinaryTree.cs
@@ -8,7 +8,38 @@ using ErrMsgs = DataStructures.ErrorMessages.ErrorMessages_US_en;
 
 namespace DataStructures.BinaryTree
 {
-    public class BinaryTree
+    public class BinaryTree<T>
     {
+        public Node<T> Root { get; set; }
+
+        public bool Insert(Node<T> node)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Find(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TraverseInOrder()
+        {
+            throw new NotImplementedException()
+        }
+
+        public void TraversePreOrder()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TraversePostOrder()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ DataStructures/BinaryTree/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.BinaryTree
+{
+    public class Node<T>
+    {
+        public T Value { get; set; }
+        public Node<T> LeftChild { get; set; }
+        public Node<T> RightChild { get; set; }
+
+    }
+}
+
