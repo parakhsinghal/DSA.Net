c934bc776308c54741fdb3be02d17c7887a9a20d
Parakh Singhal <parakh@thinkingcog.com>
2022-03-11 11:54:22 +0530

    Rectified the IsValid node return value in linked lists in the data structures project.

--- DataStructures/LinkedLists/DoublyLinkedList/Node.cs
+++ DataStructures/LinkedLists/DoublyLinkedList/Node.cs
@@ -9,7 +9,7 @@
         public Node<T> Next { get; set; }
         public Node<T> Previous { get; set; }
         public T Value { get; set; }
-        public bool IsValid { get { return this == null; } }
+        public bool IsValid { get { return this != null; } }
 
         public Node()
         {
