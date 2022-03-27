7d1e2b5e81617dea2516ca4eabc4edabeb0bd80a
Parakh Singhal <parakh@thinkingcog.com>
2022-03-27 23:05:42 +0530

    Added code for bubblesort, insertionsort, selectionsort and mergesort. Also added proper documentation in code files.

--- Algorithms/Sorting/InsertionSort.cs
+++ Algorithms/Sorting/InsertionSort.cs
@@ -6,7 +6,58 @@ using System.Threading.Tasks;
 
 namespace Algorithms.Sorting
 {
-    public class InsertionSort
+    /// <summary>
+    /// Insertion sort algorithm sorts an array in an ascending order starting from the start of the array.
+    /// The algorithm does so by creating sub-arrays from the main aray. While the main array is traversed
+    /// from left to right the sub-arrays are traversed from right to left and in the process the 
+    /// elements are swapped so that the sub-arrays will always have the smallest element to the left and
+    /// the biggest element in the right. The algo uses two loops - the outer one used to traverse through
+    /// the entire set of elements and an inner one used to traverse the sub-arrays. <br />
+    /// Time complexity: O()
+    /// </summary>
+    /// <typeparam name="T">The data type of the array that needs to be sorted.</typeparam>
+    public class InsertionSort<T>
     {
+        /// <summary>
+        /// The insertion sort algorithm sorts the elements in an ascending order with the smallest element
+        /// placed at the start of the array and the biggest at the end.<br />
+        /// Time complexity: O(n^2)
+        /// </summary>
+        /// <param name="inputArray">The array that needs to be sorted.</param>
+        /// <returns>A sorted array in which the elements are arranged in ascending order.</returns>
+        public T[] Sort(T[] inputArray)
+        {
+            int length = inputArray.Length;
+
+            //The first loop to interate over all the elements of the array from left to right.
+            for (uint i = 0; i < length - 1; i++)
+            {
+                // The second loop to iterate over the sub-array from right to left.
+                for (uint j = i + 1; j > 0; j--)
+                {
+                    switch (Comparer<T>.Default.Compare(inputArray[j - 1], inputArray[j]))
+                    {
+                        // If the right-hand element is greater then left-hand element, then move it one index value to left.
+                        case > 0:
+                            T temp = inputArray[j];
+                            inputArray[j] = inputArray[j - 1];
+                            inputArray[j - 1] = temp;
+                            break;
+
+                        // In case the right-hand element is less than the left-hand element then do nothing. 
+                        case < 0:
+                            break;
+
+                        // In case both the right-hand adn left-hand element are equal then do nothing. This also preserves the 
+                        // order of elements and makes the insertion sort stable.
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            return inputArray;
+
+        }
     }
 }
--- Algorithms/Sorting/SelectionSort.cs
+++ Algorithms/Sorting/SelectionSort.cs
@@ -7,16 +7,22 @@ using System.Threading.Tasks;
 namespace Algorithms.Sorting
 {
     /// <summary>
-    /// 
+    /// The selection sort algorithm is a sorting algorithm that sorts elements in an array in an ascending order.
+    /// The algorithm does that by traversing through all the elements in the array in two nested passes and placing the 
+    /// elements appropriately. The crux of the algorithm lies in the second pass in which the location of the smallest 
+    /// element in the sub-array is noted and then at the end of the second loop the element is appropriately
+    /// placed in the array. <br />
+    /// Time complexity: O()
     /// </summary>
-    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="T">The data type of the array.</typeparam>
     public class SelectionSort<T>
     {
         /// <summary>
-        /// 
+        /// The selection sort algorithm sorts the array in an ascending order with the smallest element
+        /// placed at the beginning of the array and the biggest at the end.
         /// </summary>
-        /// <param name="inputArray"></param>
-        /// <returns></returns>
+        /// <param name="inputArray">The array that needs to be sprted.</param>
+        /// <returns>A sorted array with elements arranged in ascending order.</returns>
         public T[] Sort(T[] inputArray)
         {
             int length = inputArray.Length;
@@ -27,14 +33,14 @@ namespace Algorithms.Sorting
                 {
                     switch (Comparer<T>.Default.Compare(inputArray[minIndex], inputArray[j]))
                     {
-                        case > 0:                           // If the element at minIndex > element at j then record index j in as minIndex.
+                        case > 0:           // If the element at minIndex > element at j then record index j in as minIndex.
                            minIndex = j;
                             break;
 
-                        case < 0:                           // If the element at minIndex < element at j then do nothing and break out of the comparison.
+                        case < 0:           // If the element at minIndex < element at j then do nothing and break out of the comparison.
                             break;
 
-                        default:                            // If both elements are equal then do nothing and break out of the comparison.
+                        default:            // If both elements are equal then do nothing and break out of the comparison.
                             break;
                     }                    
                 }
