7bd8b0ab65293bb29148f851946b644d985b9989
Parakh Singhal <parakh@thinkingcog.com>
2022-03-29 00:41:46 +0530

    Committing in the work done in the dev branch on sorting algorithms.

---
+++ Algorithms/Sorting/BubbleSort.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Sorting
+{
+    /// <summary>
+    /// Bubble sort is the most simplest way of sorting elements in an array. It compares an element 
+    /// with the rest of the elements in the array, and accordingly arranges the elements in an ascending 
+    /// order. The smallest element appears at the lowest index value in the sorted array. <br />
+    /// Time complexitY: O(n^2)
+    /// </summary>
+    /// <typeparam name="T">The data type of the array.</typeparam>
+    public class BubbleSort<T>
+    {
+        /// <summary>
+        /// Sorts the array in a naive fashion by comparing each element with the rest of the elements in the 
+        /// array in an ascending order. The smallest element in the array is placed at the left most position 
+        /// and the biggest the right-most position.
+        /// </summary>
+        /// <param name="inputArray">The array that needs to be sorted.</param>
+        /// <returns>A sorted array.</returns>
+        public T[] Sort(T[] inputArray)
+        {
+            int length = inputArray.Length;
+            for (int i = 0; i < length - 1; i++)
+            {
+                for (int j = 0; j < length - 1; j++)
+                {
+                    switch (Comparer<T>.Default.Compare(inputArray[j], inputArray[j + 1]))
+                    {
+                        case > 0:                           // If the j > j+1 then shift j+1 to one index left.
+                            T temp = inputArray[j];
+                            inputArray[j] = inputArray[j + 1];
+                            inputArray[j + 1] = temp;
+                            break;
+
+                        case < 0:                           // If the j < j+1 then do nothing and break out of the comparison
+                            break;
+
+                        default:                            // If both j and j+1 are equal then do nothing and break out of the comparison
+                            break;
+                    }
+                }
+            }
+
+            return inputArray;
+        }
+    }
+}
---
+++ Algorithms/Sorting/MergeSort.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Sorting
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class MergeSort<T>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="inputArray"></param>
+        public void Sort(int[] inputArray)
+        {
+            if (inputArray.Length == 1)
+            {
+                return;
+            }
+
+            int midIndex = inputArray.Length / 2 + inputArray.Length % 2;
+            int[] listFirstHalf = new int[midIndex];
+            int[] listSecondHalf = new int[inputArray.Length - midIndex];
+            Split(inputArray, listFirstHalf, listSecondHalf);
+
+            Sort(listFirstHalf);
+            Sort(listSecondHalf);
+
+            Merge(inputArray, listFirstHalf, listSecondHalf);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="inputArray"></param>
+        /// <param name="listFirstHalf"></param>
+        /// <param name="listSecondHalf"></param>
+        private void Split(int[] inputArray, int[] listFirstHalf, int[] listSecondHalf)
+        {
+            int index = 0;
+            int secondHalfStartIndex = listFirstHalf.Length;
+            for (int elements = 0; elements < inputArray.Length; elements++)
+            {
+                if (index < secondHalfStartIndex)
+                {
+                    listFirstHalf[index] = inputArray[index];
+                }
+                else
+                {
+                    listSecondHalf[index - secondHalfStartIndex] = inputArray[index];
+                }
+                index++;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="inputArray"></param>
+        /// <param name="listFirstHalf"></param>
+        /// <param name="listSecondHalf"></param>
+        private void Merge(int[] inputArray, int[] listFirstHalf, int[] listSecondHalf)
+        {
+            int mergeIndex = 0;
+            int firstHalfIndex = 0;
+            int secondHalfIndex = 0;
+
+            while (firstHalfIndex < listFirstHalf.Length && secondHalfIndex < listSecondHalf.Length)
+            {
+                if (listFirstHalf[firstHalfIndex] < listSecondHalf[secondHalfIndex])
+                {
+                    inputArray[mergeIndex] = listFirstHalf[firstHalfIndex];
+                    firstHalfIndex++;
+                }
+                else if (secondHalfIndex < listSecondHalf.Length)
+                {
+                    inputArray[mergeIndex] = listSecondHalf[secondHalfIndex];
+                    secondHalfIndex++;
+                }
+                mergeIndex++;
+            }
+
+            if (firstHalfIndex < listFirstHalf.Length)
+            {
+                while (mergeIndex < inputArray.Length)
+                {
+                    inputArray[mergeIndex++] = listFirstHalf[firstHalfIndex++];
+                }
+            }
+            if (secondHalfIndex < listSecondHalf.Length)
+            {
+                while (mergeIndex < inputArray.Length)
+                {
+                    inputArray[mergeIndex++] = listSecondHalf[secondHalfIndex++];
+                }
+            }
+        }
+    }
+}
